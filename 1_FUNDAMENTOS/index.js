908382a4cd7fd6aca3252876a0b80bd95f72fcee
Natan Ramalho <natan.rsd13@gmail.com>
2025-11-05 05:59:35 -0300

    Tipo number

---
+++ 1_FUNDAMENTOS/index.js
@@ -0,0 +1,7 @@
+// 1 - number
+var x = 10;
+x = 15;
+console.log(typeof x);
+console.log(x);
+var y = 10.3333;
+console.log(y.toPrecision(3));
---
+++ 1_FUNDAMENTOS/index.ts
@@ -0,0 +1,11 @@
+// 1 - number
+let x: number = 10
+
+x = 15
+
+console.log(typeof x)
+console.log(x)
+
+const y: number = 10.3333
+
+console.log(y.toPrecision(3))
