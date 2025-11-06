0d839c3ca3f751e1c6257bbe0b4e8c7862c485d6
Natan Dantas <natan.ramalho@mobilemed.com.br>
2025-11-06 11:47:30 -0300

    Type string

--- 1_FUNDAMENTOS/index.js
+++ 1_FUNDAMENTOS/index.js
@@ -5,3 +5,11 @@ console.log(typeof x);
 console.log(x);
 var y = 10.3333;
 console.log(y.toPrecision(3));
+// 2- STRING
+var fistName = "Natan";
+console.log(fistName.toUpperCase()); /// FORMATA A STRING EM CAIXA ALTA
+var lastname = "Ramalho";
+var fullName;
+fullName = fistName + " " + lastname;
+console.log(fullName);
+console.log("O tipo de full name é", typeof (fullName));
--- 1_FUNDAMENTOS/index.ts
+++ 1_FUNDAMENTOS/index.ts
@@ -9,3 +9,19 @@ console.log(x)
 const y: number = 10.3333
 
 console.log(y.toPrecision(3))
+
+// 2- STRING
+
+const fistName: string = "Natan";
+
+console.log(fistName.toUpperCase()) /// FORMATA A STRING EM CAIXA ALTA
+
+const lastname: string = "Ramalho";
+
+let fullName: string
+
+fullName = fistName + " " +  lastname
+
+console.log(fullName)
+
+console.log("O tipo de full name é", typeof(fullName))
